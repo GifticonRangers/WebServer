99d0200547deb498623853ece7abaaad2efb38f6
jang010505 <jang010505@inu.ac.kr>
2023-02-05 18:35:52 +0900

    feat: Add log

--- src/main/java/com/capstone/webserver/service/RegisterService.java
+++ src/main/java/com/capstone/webserver/service/RegisterService.java
@@ -6,10 +6,12 @@ import com.capstone.webserver.entity.Student;
 import com.capstone.webserver.entity.User;
 import com.capstone.webserver.repository.ProfessorRepository;
 import com.capstone.webserver.repository.StudentRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class RegisterService {
     @Autowired
     StudentRepository studentRepository;
@@ -18,16 +20,21 @@ public class RegisterService {
     public User register(UserForm dto) {
         if(dto.getType() == 0) {
             Student std = dto.toStudentEntity();
+            log.info("Student: {}", std.toString());
             studentRepository.save(std);
             return std;
         }
 
         else if(dto.getType() == 1){
             Professor pf = dto.toProfessorEntity();
+            log.info("Professor: {}", pf.toString());
             professorRepository.save(pf);
             return pf;
         }
 
-        else return null;
+        else {
+            log.error("Invalid request.: type Error");
+            return null;
+        }
     }
 }
