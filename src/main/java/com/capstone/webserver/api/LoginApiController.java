1c99625a261f33b1b862413f1068913fb5bfedfc
JIN <o3ouuu@inu.ac.kr>
2023-02-08 02:09:30 +0900

    style: Erase unnecessary code

--- src/main/java/com/capstone/webserver/api/LoginApiController.java
+++ src/main/java/com/capstone/webserver/api/LoginApiController.java
@@ -7,9 +7,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @Slf4j
