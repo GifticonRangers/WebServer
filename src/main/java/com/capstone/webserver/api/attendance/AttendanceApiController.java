97ca1b466b5674e7a4cbb4bb8e4bba9c583bdaf0
JIN <o3ouuu@inu.ac.kr>
2023-02-15 00:43:57 +0900

    feat: Add Attendance related files
    Co-authored-by: jang010505 <jang010505@gmail.com>

--- src/main/java/com/capstone/webserver/api/attendance/AttendanceApiController.java
+++ src/main/java/com/capstone/webserver/api/attendance/AttendanceApiController.java
@@ -1,13 +1,12 @@
 package com.capstone.webserver.api.attendance;
 
+import com.capstone.webserver.dto.user.AuditorForm;
 import com.capstone.webserver.entity.attendance.Attendance;
 import com.capstone.webserver.service.attendance.AttendanceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -17,9 +16,9 @@ public class AttendanceApiController {
     AttendanceService attendanceService;
 
 
-    @GetMapping("/api/professor/subject/createAttendanceList/{subjectId}")
-    public ResponseEntity<ArrayList<Attendance>> createAttendanceList(@PathVariable Long subjectId) {
-        ArrayList<Attendance> attendanceArrayList = attendanceService.createAttendanceList(subjectId);
+    @PostMapping("/api/professor/subject/createAttendanceList/")
+    public ResponseEntity<ArrayList<Attendance>> createAttendanceList(@RequestBody AuditorForm dto) {
+        ArrayList<Attendance> attendanceArrayList = attendanceService.createAttendanceList(dto.getIdSubject(), dto.getId());
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .build();
--- src/main/java/com/capstone/webserver/service/attendance/AttendanceService.java
+++ src/main/java/com/capstone/webserver/service/attendance/AttendanceService.java
@@ -1,32 +1,65 @@
 package com.capstone.webserver.service.attendance;
 
 import com.capstone.webserver.entity.attendance.Attendance;
+import com.capstone.webserver.entity.attendance.State;
 import com.capstone.webserver.entity.subject.Subject;
+import com.capstone.webserver.entity.user.Auditor;
+import com.capstone.webserver.entity.user.User;
 import com.capstone.webserver.repository.AttendanceRepository;
+import com.capstone.webserver.repository.AuditorRepository;
 import com.capstone.webserver.repository.SubjectRepository;
+import com.capstone.webserver.repository.UserRepository;
 import com.capstone.webserver.util.DateUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Map;
 
 @Slf4j
 @Service
 public class AttendanceService {
     @Autowired
     AttendanceRepository attendanceRepository;
+
     @Autowired
     SubjectRepository subjectRepository;
 
-    public ArrayList<Attendance> createAttendanceList(Long subjectId) {
+    @Autowired
+    AuditorRepository auditorRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    public ArrayList<Attendance> createAttendanceList(Long subjectId, Long professorId) {
         Subject target = subjectRepository.findById(subjectId).orElse(null);
         if (target != null) {
-            DateUtil.computeDate(
+            Map<Integer, ArrayList<String>> schedules = DateUtil.computeDate(
                     Integer.parseInt(target.getYearSubject()),
                     target.getSemesterSubject(),
                     target.getTimeSubject()
             );
+
+            ArrayList<Auditor> auditors = auditorRepository.findAllByIdSubject(subjectId);
+
+            for(Auditor auditor: auditors){
+                for (int i = 1; i <= 16; i++) {
+                    for (String schedule: schedules.get(i)){
+                        attendanceRepository.save(
+                                Attendance
+                                        .builder()
+                                        .id(null)
+                                        .dateAttendance(schedule)
+                                        .stateAttendance(State.ATTENDANCE)
+                                        .idProfessor(professorId)
+                                        .idStudent(auditor.getIdStudent())
+                                        .idSubject(subjectId)
+                                        .build());
+                    }
+                }
+            }
 
             return null;
         }
