5b3cce47de6df8682b9621cffac3ca33916e174d
JIN <o3ouuu@inu.ac.kr>
2023-02-15 00:42:46 +0900

    feat: Add DateUtil related files
    Co-authored-by: jang010505 <jang010505@gmail.com>

--- src/main/java/com/capstone/webserver/util/DateUtil.java
+++ src/main/java/com/capstone/webserver/util/DateUtil.java
@@ -3,12 +3,46 @@ package com.capstone.webserver.util;
 import java.util.*;
 
 public class DateUtil {
-    public static ArrayList<Map<Integer, String>> computeDate(int year, String semester, String time){
+    public static Map<Integer, ArrayList<String>> computeDate(int year, String semester, String time){
         int month = (Objects.equals(semester, "1학기") ? Calendar.MARCH : Calendar.SEPTEMBER);
         int day = (Objects.equals(semester, "1학기") ? 2 : 1);
         Calendar startDay = new GregorianCalendar(year, month, day);
 
+        Map<String, ArrayList<String>> splitSubjectTime = SubjectTimeUtil.splitSubjectTime(time);
 
-        return null;
+        /*
+         * 1단계: 요일 배열 만들기
+         * 대충 월 ~ 일까지
+         */
+        ArrayList<String> week = new ArrayList<String>(Arrays.asList("일", "월", "화", "수", "목", "금", "토"));
+
+        /*
+         * 2단계: startDay에서 요일 받아와서 요일 배열 정렬하기
+         * 함수로 만들면 이쁠듯 1단계랑 같이 ㅋㅋ
+         */
+        int dayOfWeek = startDay.get(Calendar.DAY_OF_WEEK);
+
+        for (int i = 0; i < dayOfWeek-1; i++) {
+            String temp = week.get(0);
+            week.remove(0);
+            week.add(temp);
+        }
+
+        /*
+         * 3단계: for문 돌면서 attendance에 시간 추가하기
+         */
+        Map<Integer, ArrayList<String>> attendance = new HashMap<>();
+
+        for (int i = 1; i <= 16; i++) {
+            attendance.put(i, new ArrayList<String>());
+
+            for (String w : week) {
+                if(splitSubjectTime.containsKey(w))
+                    for (String t : splitSubjectTime.get(w))
+                        attendance.get(i).add(w+t);
+            }
+        }
+
+        return attendance;
     }
 }
---
+++ src/test/java/com/capstone/webserver/util/DateUtilTest.java
@@ -0,0 +1,14 @@
+package com.capstone.webserver.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DateUtilTest {
+
+    @Test
+    void computeDate(){
+        assertEquals(DateUtil.computeDate(2023, "1학기", " [SP116:월(6), 금(7-8A)] [SP403:수(3)]"), "");
+    }
+}
