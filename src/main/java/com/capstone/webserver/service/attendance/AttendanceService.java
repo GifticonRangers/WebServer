8c81d1e7694366c346df1bd1c422fe6ff71131f1
JIN <o3ouuu@inu.ac.kr>
2023-04-24 23:29:59 +0900

    style: Cleanup imports

--- src/main/java/com/capstone/webserver/service/attendance/AttendanceService.java
+++ src/main/java/com/capstone/webserver/service/attendance/AttendanceService.java
@@ -20,7 +20,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Map;
-import java.util.Optional;
 
 import static com.capstone.webserver.config.error.ErrorCode.*;
 
