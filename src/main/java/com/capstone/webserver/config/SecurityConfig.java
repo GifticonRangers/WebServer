4fb137f80f13de5f06c28f8184dfec61f38e86fd
JIN <o3ouuu@inu.ac.kr>
2023-04-26 21:16:54 +0900

    feat: Add cors Option

--- src/main/java/com/capstone/webserver/config/SecurityConfig.java
+++ src/main/java/com/capstone/webserver/config/SecurityConfig.java
@@ -16,6 +16,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -28,7 +33,7 @@ public class SecurityConfig  {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors().disable()
+                .cors().and()
                 .csrf().disable() // post 방식으로 값을 전송할 때 token을 사용해야 하는 보안 설정 해제
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
@@ -41,31 +46,21 @@ public class SecurityConfig  {
                 .exceptionHandling()
                 .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
                 ;
-//                .addFilterBefore(new JWTExceptionFilter(), TokenAuthenticationFilter.class);
 
-        /*
-        http
-                .authorizeRequests()
-                    .antMatchers("/**").permitAll()
-                    // 권한에 따라 아래 경로에만 접속가능하게끔 설정
-                    //.antMatchers("/**").hasAnyRole("ADMIN")
-                    //.antMatchers("/api/professor/**").hasAnyRole("PROFESSOR")
-                    //.antMatchers("/api/student/**").hasAnyRole("STUDENT")
-                    // 그 외 모든 리소스는 인증이 필요
-                    .anyRequest().authenticated();
-        http
-                .logout()
-                    .permitAll()
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/api/logout")) // Default log-out page, 주소창에 요청해도 POST로 인식
-                    // redirect 하지 않고 바로 logout 처리
-                    .logoutSuccessHandler(((request, response, authentication) -> {
-                        response.setStatus(HttpServletResponse.SC_OK);
-                    }))
-                    .deleteCookies("JSESSIONID") // Delete JESSIONID on log-out
-                    .invalidateHttpSession(true) // End Session on log-out
-                    .clearAuthentication(true);  // Clear Authentication on log-out
-        */
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 
     @Bean
